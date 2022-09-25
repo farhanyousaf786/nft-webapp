751bf7c83cac885d3d8e7c81908bbb7433d82da7
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-09-25 07:33:23 -0400

    added signup page as smenit ui

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-
+import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
 import userService from "../../utils/userService";
 import { useNavigate } from "react-router-dom";
 
@@ -15,18 +15,76 @@ export default function SignUpPage(props) {
     bio: "",
   });
 
+  function handleChange(e) { 
+  }
+  function handleFileInput(e) {
+  }
 
-
+  async function handleSubmit(e) {}
   return (
-    <>
-      <h1>Signup PAGE</h1>
-      <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Login Controller, to know how it is setup to
-          find the user!
-        </li>
-      </ul>
-    </>
+
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h2" color="teal" textAlign="center">
+         Sign Up Page
+      </Header>
+      <Form autoComplete="off" onSubmit={handleSubmit}>
+        <Segment stacked>
+          <Form.Input
+            name="username"
+            placeholder="username"
+            value={state.username}
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={state.email}
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+            name="password"
+            type="password"
+            placeholder="password"
+            value={state.password}
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+            name="passwordConf"
+            type="password"
+            placeholder="Confirm Password"
+            value={state.passwordConf}
+            onChange={handleChange}
+            required
+          />
+          <Form.TextArea
+            label="bio"
+            name="bio"
+            placeholder="Tell us more about your dogs..."
+            value={state.bio}
+
+            onChange={handleChange}
+          />
+          <Form.Field>
+            <Form.Input
+              type="file"
+              name="photo"
+              placeholder="upload image"
+              onChange={handleFileInput}
+            />
+          </Form.Field>
+          <Button type="submit" className="btn">
+            Signup
+          </Button>
+        </Segment>
+        {error ? <ErrorMessage error={error} /> : null}
+      </Form>
+    </Grid.Column>
+  </Grid>
+
   );
 }
