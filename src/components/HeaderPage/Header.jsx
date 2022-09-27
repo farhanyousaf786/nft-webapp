276037d84f45765b2a7948b1e5f62feecc130631
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-09-27 18:38:41 -0400

    add navbar

--- src/components/HeaderPage/Header.jsx
+++ src/components/HeaderPage/Header.jsx
@@ -1,66 +1,66 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Header, Segment, Image, Icon, Table } from "semantic-ui-react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { Header, Segment, Image, Icon, Table } from "semantic-ui-react";
 
-export default function HeaderPage({ loggedUser, handleLogout }) {
-  return (
+// export default function HeaderPage({ loggedUser, handleLogout }) {
+//   return (
 
-    <Segment clearing >
+//     <Segment clearing >
 
-      <Header as="h2" floated="right">
-        <Link to={`/${loggedUser?.username}`}>
+//       <Header as="h2" floated="right">
+//         <Link to={`/${loggedUser?.username}`}>
           
-          <Image
-            src={
-              loggedUser?.photoUrl
-                ? loggedUser?.photoUrl
-                : "https://react.semantic-ui.com/images/wireframe/square-image.png"
-            }
-            avatar></Image>
+//           <Image
+//             src={
+//               loggedUser?.photoUrl
+//                 ? loggedUser?.photoUrl
+//                 : "https://react.semantic-ui.com/images/wireframe/square-image.png"
+//             }
+//             avatar></Image>
 
 
-        </Link>
-        {loggedUser ? (
-          <Link to="" onClick={handleLogout}>
-            Logout
-          </Link>
-        ) : (
-          <Link to={`/${loggedUser?.username}`}>Login</Link>
-        )}
+//         </Link>
+//         {loggedUser ? (
+//           <Link to="" onClick={handleLogout}>
+//             Logout
+//           </Link>
+//         ) : (
+//           <Link to={`/${loggedUser?.username}`}>Login</Link>
+//         )}
         
-      </Header>
+//       </Header>
 
 
-      <Header as="h2" >
+//       <Header as="h2" >
        
 
-       {loggedUser ? (
-       <>
-      username =  {loggedUser.username}
+//        {loggedUser ? (
+//        <>
+//       username =  {loggedUser.username}
 
-       </>
-       ) : (
-       null
-       )}
-     </Header>
+//        </>
+//        ) : (
+//        null
+//        )}
+//      </Header>
    
 
 
-      <Header as="h2" floated="left">
-        <Link to="/">
-          <Icon name="home"></Icon>
-        </Link>
+//       <Header as="h2" floated="left">
+//         <Link to="/">
+//           <Icon name="home"></Icon>
+//         </Link>
 
-        {loggedUser ? (
-        <>
-            <Link to="/">
-            Home
-            </Link>
-        </>
-        ) : (
-        null
-        )}
-      </Header>
+//         {loggedUser ? (
+//         <>
+//             <Link to="/">
+//             Home
+//             </Link>
+//         </>
+//         ) : (
+//         null
+//         )}
+//       </Header>
 
 
 
@@ -68,6 +68,50 @@ export default function HeaderPage({ loggedUser, handleLogout }) {
    
 
 
-    </Segment>
+//     </Segment>
+//   );
+// }
+
+
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './HeaderElements';
+
+const HeaderPage = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <h1>Home</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/services' activeStyle>
+            Services
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to='/login' activeStyle>
+            Sign Up
+          </NavLink>
+         
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   );
-}
+};
+
+export default HeaderPage;
