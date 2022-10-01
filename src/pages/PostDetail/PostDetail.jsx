3a2d1fc9b618b01c43e712578840f48cdb5efab5
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-01 12:11:57 -0400

    added loader and error message and setstate

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -1,39 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import postdetail from "../../utils/postdetail";
-import { useParams }  from 'react-router-dom'
+import { useParams }  from 'react-router-dom';
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import PostGallery from "../../components/PostGallery/PostGallery";
+import PageHeader from "../../components/HeaderPage/Header";
+import Loading from "../../components/Loader/Loader";
+
 
 
 export default function PostDetail() {
 
 
 const {id} = useParams();
+const [post, setPost] = useState([]);
+const [error, setError] = useState("");
+const [loading, setLoading] = useState(true);
+
+
 
 
 
   useEffect(()=>{
 
-
     async function postDetail() {
       try {
         console.log("id in Deatils page ", id) 
 
-        const reponse = await postdetail.getDetails(id);
-        console.log("response in Deatils page ", reponse) 
+        const response = await postdetail.getDetails(id);
+
+        console.log("post -- >> ", response.data.post);
+
+        setLoading(false);
+
+        setPost(response.data.post);
 
 
       }catch(err){
 
         console.log("Error in post detail response >> ", err) 
+        setError("Post does not exist, Refresh the page or go back"); // < this is message we leave the user
+
 
       };
 
     }
 
-
     postDetail();
 
   },[id])
+
+  if (error) {
+    return (
+      <>
+        <ErrorMessage error={error} />;
+      </>
+    );
+  }
+
+  if (loading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+
+
 
   return (
     <>
